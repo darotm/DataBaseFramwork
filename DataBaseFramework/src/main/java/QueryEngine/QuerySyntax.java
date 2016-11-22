5f7c12279a8d412220b39bcbd2a37602d8290966
JF225657 <JF225657@PC0027888.warta.ad.intra>
2016-11-22 16:00:22 +0100

    Dodanie metody do ubsługi wyników sql

---
+++ DataBaseFramework/src/main/java/QueryEngine/QuerySyntax.java
@@ -0,0 +1,13 @@
+package QueryEngine;
+import QueryEngine.Query;
+
+public class QuerySyntax {
+		
+	ResultSet Result = QueryEngine.Query.
+	    
+		
+		while(rs.next()){
+			System.out.println(Result.getString("ELEMENT_ID"));
+			scalenieResult = rs.getString("ELEMENT_ID");
+}
+}
